d5ffdd6173bb4aabb9b91aff7c27e113fcb0a473
a4-01 <clachic00@naver.com>
2020-04-02 17:47:30 +0900

    okok

---
+++ oop1.py
@@ -0,0 +1,23 @@
+
+class Circle:
+    def __init__(self):
+        print("Circle init")
+        self.radius = 10
+
+    def show(self):
+        print("show", self.radius)
+
+
+
+c1 = Circle()
+c2 = Circle()
+
+print(c1)
+print(c1.radius)
+print(c2)
+print(c2.radius)
+
+c1.radius = 5
+c1.show()
+c2.radius = 10
+c2.show()
